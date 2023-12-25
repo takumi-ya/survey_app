bce584c3b7fe81097ad9ffa17faddad96b536f48
Takumi Yamada <takumiyamada@TakumiMacBook-Air.local>
2023-12-25 23:16:01 +0900

    Add AuthController and AuthControllerProvider

---
+++ lib/view_model/auth_controller.dart
@@ -0,0 +1,54 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class AuthController extends StateNotifier<User?> {
+  AuthController({User? initialUser}) : super(initialUser) {
+    _auth.userChanges().listen((user) {
+      state = user;
+    });
+  }
+
+  final _auth = FirebaseAuth.instance;
+
+  Future<void> signInWithAnonymous() async {
+    final userCredential = await _auth.signInAnonymously();
+    debugPrint('[info] logged in: ${userCredential.user?.uid}');
+    state = userCredential.user;
+  }
+
+  Future<void> signInWithGoogle() async {
+    try {
+      final googleUser = await GoogleSignIn().signIn();
+      final googleAuth = await googleUser!.authentication;
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      final userCredential =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+
+      debugPrint('[info] logged in: ${userCredential.user?.uid}');
+
+      state = userCredential.user;
+    } catch (e) {
+      debugPrint('[error] failed to login: $e');
+    }
+  }
+
+  // Tips: Anonymousアカウントはサインアウトできない
+  Future<void> signOut() async {
+    await _auth.signOut();
+    state = null;
+  }
+
+  Future<void> deleteAccount() async {
+    await state?.delete();
+  }
+
+  void logout() {
+    state = null;
+  }
+}
